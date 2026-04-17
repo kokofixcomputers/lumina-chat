8260192240c430422f68b571bc431fe16c8fbb38
kokodev <koko@kokodev.cc>
2026-04-17 11:57:48 -0700

    fix hash

--- api/hash.ts
+++ api/hash.ts
@@ -3,7 +3,7 @@ export const config = {
 };
 
 export default function handler(req: Request) {
-  const sha = process.env.VITE_VERCEL_GIT_COMMIT_SHA ?? null;
+  const sha = process.env.VERCEL_GIT_COMMIT_SHA ?? null;
   return new Response(JSON.stringify({ sha }), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
